32b1900dd8b96802cc65283b66d6aa5e864a8dc2
Levy <85988919231levy@gmail.com>
2024-11-26 06:51:57 -0300

    estilizzando page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,41 +2,63 @@
 import React from "react";
 import InputText from "../../Components/Form/Input/Input";
 import Buttom from "../../Components/Form/Buttom/Buttom";
+import './Login.css'
 
 function Login () {
 
     return (
         <>
-    <form action="">
-        <InputText
-            type = "text"
-            place= "Nome"
-            class= "bi bi-person"
-            required = "true"
 
-        />
+    <div className="container">
 
-        <InputText
-            type = "text"
-            place= "Email"
-            class= "bi bi-envelope"
-            required = "true"
+        <div className="container-left">
+            <h1>Levy Tech</h1>
+
+            <h3>Bem-Vindo</h3>
+            <h3>de volta!</h3>
+
+            <p>Acesse sua conta agora mesmo.</p>
+
+            <Buttom
+                type = "submit"
+                text = "Entrar"
+            />
+        </div>
+
+        <div className="container-rigth">
+
+            <form action="">
+                <InputText
+                    type = "text"
+                    place= "Nome"
+                    class= "bi bi-person"
+                    required = "true"
+
             />
 
-        <InputText
-            type = "text"
-            place = "Senha"
-            class = "bi bi-file-lock"
-            required = "true"
+                <InputText
+                    type = "text"
+                    place= "Email"
+                    class= "bi bi-envelope"
+                    required = "true"
+                />
 
+                <InputText
+                    type = "password"
+                    place = "Senha"
+                    class = "bi bi-file-lock"
+                    required = "true"
+            />
+
+                <Buttom
+                    type = "submit"
+                    text = "Cadastrar"
+                />
+            </form>
+        </div>
+    </div>
         
-        />
 
-        <Buttom
-            type = "submit"
-            text = "Cadastrar"
-            />
-    </form>
         </>
         
     )
